636d15c2976b156ed1004c9a97b1655e817500bb
广美 <15710007569@163.com>
2017-08-31 12:08:25 +0800

    text项目

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 广美 on 2017/8/31.
+//  Copyright © 2017年 广美. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
